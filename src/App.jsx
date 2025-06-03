b7f3de4fb2df4e547c936a69dcd7459a92e5c515
vikrum96 <vikrumravi96@gmail.com>
2025-06-02 19:22:07 -0500

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  
-    
-     
-  
+  return (
+    <>
+      <div>
+        <h1>Aggie Track</h1>
+        <div>Favorites</div>
+        <button>Dark Mode</button>
+      </div>
+
+      <div />
+
+      <div>Course Major</div>
+
+      <form>
+        <input />
+      </form>
+
+      <button>Search</button>
+      
+
+      <div />
+
+      <div>Course Number</div>
+
+      <form>
+        <input />
+      </form>
+
+      <button>Search</button>
+    </>
+  );
 }
 
-export default App
+export default App;
